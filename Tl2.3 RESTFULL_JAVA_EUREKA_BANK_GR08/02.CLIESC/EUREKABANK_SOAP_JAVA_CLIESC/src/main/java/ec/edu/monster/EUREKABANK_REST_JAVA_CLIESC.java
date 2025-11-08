2f2555dd5d01435799caf16c43e05b09896d3bf7
Jcnazate <jcnazate@hotmail.com>
2025-11-08 18:46:44 -0500

    Taller 2.3 escritorio

---
+++ Tl2.3 RESTFULL_JAVA_EUREKA_BANK_GR08/02.CLIESC/EUREKABANK_SOAP_JAVA_CLIESC/src/main/java/ec/edu/monster/EUREKABANK_REST_JAVA_CLIESC.java
@@ -0,0 +1,12 @@
+package ec.edu.monster;
+
+import ec.edu.monster.controller.LoginController;
+import ec.edu.monster.view.LoginView;
+
+public class EUREKABANK_REST_JAVA_CLIESC {
+
+    public static void main(String[] args) {
+        LoginView loginView = new LoginView();
+        loginView.setVisible(true);
+    }
+}
